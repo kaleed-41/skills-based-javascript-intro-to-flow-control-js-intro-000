5ee8d4b1011241e0e340b82715d5e1e385ba02c7
Daniel Williams <danielwil621@gmail.com>
2021-03-29 12:04:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,6 +15,9 @@ function ageChecker(age) {
   if(age > 13 && age < 19){
     return "You are a teenager!";
   }
+  else if (age <= 12) {
+    return "You are a kid";
+  }
 }
 
 function ternaryTeenager(age) {
